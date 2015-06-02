6af030474d55016a64d7e05c5b5ee35b089cff48
raduy <raduj.lukasz@gmail.com>
2015-06-02 11:13:35 +0200

    Bootstrapping

---
+++ src/main/java/pl/agh/tai/App.java
@@ -0,0 +1,20 @@
+package pl.agh.tai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
