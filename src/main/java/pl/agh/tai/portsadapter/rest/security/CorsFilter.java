97f17d7fe9f139dd06aef8576db2e24d936ee329
sin6pi7 <jac.zyla@gmail.com>
2015-06-29 23:42:17 +0200

    Allow credentials, change allowed domains

--- src/main/java/pl/agh/tai/portsadapter/rest/security/CorsFilter.java
+++ src/main/java/pl/agh/tai/portsadapter/rest/security/CorsFilter.java
@@ -19,9 +19,10 @@ public class CorsFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization, Authorization");
         chain.doFilter(req, res);
     }
